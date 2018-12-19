dec54a43fb1100f67c07030ba2ce3d48996353fa
Meri Saarinen <mtmsaa@utu.fi>
2018-12-19 15:02:59 +0200

    Added check marks to a select row

--- src/components/stockData.js
+++ src/components/stockData.js
@@ -7,7 +7,7 @@ export const Stock = (props) => {
       <td><p>{props.stock.uv}</p></td>
       <td><p>{props.stock.quantity}</p></td>
       <td><p>{props.stock.tv}</p></td>
-
+      <td><input type="checkbox" name="same" ></input></td>
     </tr>
     
   )
