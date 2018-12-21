3979baeaa051b2e0d51ad914265319eb3c46fae6
Meri Saarinen <mtmsaa@utu.fi>
2018-12-21 22:16:37 +0200

    Updates

---
+++ precache-manifest.da58ec18921abfb20ac1b22b5bc15fec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf5d1957c0ec851a480e",
+    "url": "/2018-interactive-web-apps-project-merisaar/static/js/runtime~main.bf5d1957.js"
+  },
+  {
+    "revision": "9eb33960e3056f434964",
+    "url": "/2018-interactive-web-apps-project-merisaar/static/js/main.9eb33960.chunk.js"
+  },
+  {
+    "revision": "15e229018599907f6e5e",
+    "url": "/2018-interactive-web-apps-project-merisaar/static/js/1.15e22901.chunk.js"
+  },
+  {
+    "revision": "9eb33960e3056f434964",
+    "url": "/2018-interactive-web-apps-project-merisaar/static/css/main.f173817d.chunk.css"
+  },
+  {
+    "revision": "15e229018599907f6e5e",
+    "url": "/2018-interactive-web-apps-project-merisaar/static/css/1.78373d4d.chunk.css"
+  },
+  {
+    "revision": "13c48e7166cf3f17f1b98164508b0a3a",
+    "url": "/2018-interactive-web-apps-project-merisaar/index.html"
+  }
+];
